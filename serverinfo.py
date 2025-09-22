097e5024c17db3f3b4b88c6b36f2e39470e9f6c3
HookDev-arch <ip.docenko_v.a@bk.ru>
2025-09-22 16:42:20 +0600

    Update serverinfo.py

--- serverinfo.py
+++ serverinfo.py
@@ -1,4 +1,4 @@
-__version__ = (2, 0, 1)
+__version__ = (3, 3, 0)
 
 # meta developer: @hookdev_arch
 # meta icon: https://example.com/moodmirror_icon.png
@@ -88,4 +88,5 @@ class serverInfoMod(loader.Module):
         }
 
         await utils.answer(message, self.strings("servinfo").format(**inf))
+
 
